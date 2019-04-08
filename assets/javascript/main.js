1b0a80d2db2a4f854a6f4e3dfd4b61d0aa3312f8
Mday1313 <melissaday076@gmail.com>
2019-04-07 23:22:59 -0400

    basic html complete

---
+++ assets/javascript/main.js
@@ -0,0 +1,35 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyALuPniOc_Uxf9zEK_ebJDBC05HWqHyUB4",
+    authDomain: "rpg-multiplayer-effc6.firebaseapp.com",
+    databaseURL: "https://rpg-multiplayer-effc6.firebaseio.com",
+    projectId: "rpg-multiplayer-effc6",
+    storageBucket: "",
+    messagingSenderId: "508174102668"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+// Firebase used to allow only two players, watch when player comes and goes and enable chat
+//   Player one logs in
+//  Hide "enter name" display player one name
+// Player two logs in 
+// Hide "enter name" display player two name
+
+// If 2 players logged in
+    // Display choose R P or S
+
+
+// on click log each players choice
+    // compare choices
+    // determine winner
+    // Display results text
+    // add win or tie to players score
+
+
+// if player leaves rest game and score
+
+
+
+
